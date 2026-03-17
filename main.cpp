2c45c966a1df2483f3e2f5a438800a4ba75466db
Михаил Лачугин <mikee@MacBook-Air-Mihail.local>
2026-03-17 13:35:42 +0300

    make tests in main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
 #include "vector.hpp"
-int main()
+bool testDefaultVector()
 {
+  topit::Vector< int > v;
+  return v.isEmpty();
+}
 
+bool testVectorWithValue() {
+  topit::Vector< int > v;
+}
+
+int main() {
+  ///std::cout << "Def vect should be empty: " << testDefaultVector() << "\n";
+  using test_t = bool(*)();
+  using pair_t = std::pair< const char*, test_t>;
+  pair_t tests[] {
+    {"Def vect is empty", testDefaultVector},
+    {" Vect with any value is not empty", testVectorWithValue}
+  };
+
+  const size_t count = sizeof(tests) / sizeof(pair_t);
+  std::cout << std::boolalpha;
+  for (size_t i = 0; i < count; ++i) {
+    bool res = tests[i].second();
+    std::cout << tests[i].first << " : " << res << "\n";
+  }
 }
--- vector.hpp
+++ vector.hpp
@@ -5,10 +5,33 @@ namespace topit
 {
   template <class T>
 struct Vector {
+    Vector();
+    Vector(const Vector< T >&) = default;
+    ~Vector();
+    Vector< T >& operator=(const Vector < T >&) = delete;
 
+    void pushBacl(const T&);
+    bool isEmpty() const noexcept;
   private:
     T* data;
     size_t size_, capasity_;
   };
+
 }
+template<class T>
+bool topit::Vector<T>::isEmpty() const noexcept {
+  return false;
+}
+
+template<class T>
+topit::Vector<T>::~Vector() {
+  delete[] data;
+}
+
+
+template<class T>
+topit::Vector<T>::Vector():
+  data(nullptr), size_(0), capasity_(0)
+{}
+
 #endif
