29ec731cf95d1fd6b679bbdcfb78d950da0005f8
Михаил Лачугин <mikee@MacBook-Air-Mihail.local>
2026-03-17 12:41:30 +0300

    add vector.hpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-
-int main() {
+#include "vector.hpp"
+int main()
+{
 
 }
---
+++ vector.hpp
@@ -0,0 +1,14 @@
+#ifndef VECTOR_HPP
+#define VECTOR_HPP
+#include <iostream>
+namespace topit
+{
+  template <class T>
+struct Vector {
+
+  private:
+    T* data;
+    size_t size_, cap;
+  };
+}
+#endif
